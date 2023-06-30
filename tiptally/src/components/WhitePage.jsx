701f32da2f0fc8fa2dcbfbfc8be96cf76360a6b1
thatkhay <johnkingsleyegeonu@gmail.com>
2023-06-30 15:17:16 +0100

    white page

---
+++ tiptally/src/components/WhitePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WhitePage() {
+  return (
+    <div style={{backgroundColor: 'hsl(0, 0%, 100%)', height:'70%', width: '60%', borderRadius: '1rem', display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '1.5rem'}}>
+       
+    </div>
+  )
+}
+
+export default WhitePage
---
+++ tiptally/src/pages/Mypage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import WhitePage from '../components/WhitePage'
+
+function Mypage() {
+
+
+  return (
+    <div style={{height: '100vh', display: 'flex' , alignItems: 'center', justifyContent: 'center', backgroundColor: 'hsl(185, 41%, 84%)'}}>
+     <WhitePage />
+    </div>
+  )
+}
+
+export default Mypage
