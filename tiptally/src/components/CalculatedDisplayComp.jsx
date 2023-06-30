e7ac16e0096064925fe7b994f645e3f46b61e347
thatkhay <johnkingsleyegeonu@gmail.com>
2023-06-30 22:47:24 +0100

    created a  componet where calculated tip displays

--- tiptally/src/components/CalculatedDisplayComp.jsx
+++ tiptally/src/components/CalculatedDisplayComp.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Button from '@mui/material/Button';
 
 function CalculatedDisplayComp() {
+
+  
   return (
-    <div style={{ width: '50%', marginLeft: '1rem'}}>CalculatedDisplayComp</div>
+    <div style={{ width: '50%', marginLeft: '1rem', backgroundColor: 'hsl(183, 100%, 15%) ' , height: '100%', padding: '1rem', borderRadius: '.7rem'}}>
+     
+     <div style={{ display: 'flex', alignItems: 'center' , justifyContent: 'space-between'}}>
+      <div>
+       <h5 style={{fontSize: '.7rem', color: 'white', textTransform: 'uppercase', textAlign: 'left'}}>tip amount</h5>
+       <span style={{fontSize: '.6rem', color: 'hsl(189, 41%, 97%)', textAlign: 'left' , marginLeft: '-2rem' }}>/person</span>
+      </div >
+      <span style={{fontSize: '2rem', color: 'hsl(172, 67%, 45%)'}}>$</span>
+     </div>
+
+
+     
+     <div style={{ display: 'flex', alignItems: 'center' , justifyContent: 'space-between', marginTop: '2rem'}}>
+      <div>
+      <h5 style={{fontSize: '.7rem', color: 'white', textTransform: 'uppercase', textAlign: 'left'}}>total</h5>
+       <span style={{fontSize: '.6rem', color: 'hsl(189, 41%, 97%)', textAlign: 'left' }}>/person</span>
+      </div>
+      <span style={{fontSize: '2rem', color: 'hsl(172, 67%, 45%)'}}>$</span>
+     </div>
+     
+     <Button  variant="contained" style={{ backgroundColor: 'hsl(172, 67%, 45%)', height: '2rem', width: '70%', color: 'hsl(183, 100%, 15%)', marginTop: '3rem' }}>reset</Button>
+    </div>
   )
 }
 
--- tiptally/src/components/InputAndCalcComp.jsx
+++ tiptally/src/components/InputAndCalcComp.jsx
@@ -8,18 +8,18 @@ function InputAndCalcComp() {
     <div style={{ display: 'flex', flexDirection: 'column', width: '50%', marginRight: '1rem' }}>
       <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'left' }}>
         <label htmlFor="number" style={{ textTransform: 'capitalize', color: 'hsl(186, 14%, 43%)' }}>bill</label>
-        <input type="number" style={{ backgroundColor: 'hsl(189, 41%, 97%)', border: '0', borderRadius: '.2rem', height: '1.7rem', width: '20rem', marginTop: '1rem', marginLeft: '1rem' }} />
+        <input type="number" style={{fontFamily: 'Space Mono', backgroundColor: 'hsl(189, 41%, 97%)', border: '0', borderRadius: '.2rem', height: '1.7rem', width: '20rem', marginTop: '1rem', marginLeft: '1rem' , textAlign: 'right' }} />
       </div>
 
       <div style={{ textAlign: 'left', display: 'flex', flexDirection: 'column', marginTop: '2rem' }}>
         <label htmlFor="number" style={{ textTransform: 'capitalize', color: 'hsl(186, 14%, 43%)' }}>select tip</label>
-        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '1rem' }}>
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 30%)', gap: '1rem' }}>
           {tipVal.map((tip) => (
             <Button key={tip} variant="contained" style={{ backgroundColor: 'hsl(183, 100%, 15%)', marginTop: '1rem' }}>
               {tip} %
             </Button>
           ))}
-          <input type="text" placeholder="Custom" style={{ fontFamily: 'Space Mono',  backgroundColor: 'hsl(189, 41%, 97%)', border: '0', borderRadius: '.4rem', height: '2.3rem', width: '6rem', marginTop: '1rem' , textAlign: 'right' , padding: '0 0.5rem'}} />
+          <input type="text" placeholder="Custom" style={{ fontFamily: 'Space Mono',  backgroundColor: 'hsl(189, 41%, 97%)', border: '0', borderRadius: '.4rem', height: '2.3rem', width: '5.7rem', marginTop: '1rem' , textAlign: 'right' , padding: '0 0.5rem'}} />
         </div>
       </div>
 
@@ -28,7 +28,7 @@ function InputAndCalcComp() {
             <h5 style={{textTransform: 'capitalize'}}>number of people</h5>
             <h5 style={{textTransform: 'capitalize'}}>n</h5>
           </div>
-          <input type="number" style={{ backgroundColor: 'hsl(189, 41%, 97%)', border: '0', borderRadius: '.2rem', height: '1.7rem', width: '20rem', marginLeft: '1rem', fontSize: '2rem' }} />
+          <input type="number" style={{fontFamily: 'Space Mono', backgroundColor: 'hsl(189, 41%, 97%)', border: '0', borderRadius: '.2rem', height: '1.7rem', width: '20rem', marginLeft: '1rem', textAlign: 'right' }} />
       </div>
 
     </div>
